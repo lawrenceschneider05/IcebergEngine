e684cde66844e92d719d20eac50e7c1e400206e7
Lars Schneider <schneila@oregonstate.edu>
2026-08-17 14:26:20 -0700

    Add project files.

---
+++ IcebergEngine/IcebergEngine.cpp
@@ -0,0 +1,12 @@
+﻿// IcebergEngine.cpp : Defines the entry point for the application.
+//
+
+#include "IcebergEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
