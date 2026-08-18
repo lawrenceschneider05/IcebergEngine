68ebf9ad4930329944774321670338a0dea7f14b
bigman79 <64438880+bigman79@users.noreply.github.com>
2026-08-17 18:43:50 -0700

    Unit Testing!!!!

--- src/engine/Scene/Entity.h
+++ src/engine/Scene/Entity.h
@@ -4,9 +4,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace Engine {
-	
+
 	struct Transform {
-		
+
 		glm::vec3 position{ 0.0f, 0.0f, 0.0f };
 		glm::vec3 rotation{ 0.0f, 0.0f, 0.0f };
 		glm::vec3 scale{ 1.0f, 1.0f, 1.0f };
@@ -15,17 +15,20 @@ namespace Engine {
 
 	class Entity {
 	public:
-		Entity(const Transform& transform)
-		: _transform{ transform }{}
+		Entity(Transform& transform)
+			: _transform{ transform } {
+		}
 
-		const glm::mat4 getModelMatrix() { glm::mat4 model = glm::translate(glm::mat4(1.0f), _transform.position);
+		const glm::mat4 getModelMatrix() const {
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), _transform.position);
 			model = glm::rotate(model, glm::radians(_transform.rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
 			model = glm::rotate(model, glm::radians(_transform.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
 			model = glm::rotate(model, glm::radians(_transform.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
 			model = glm::scale(model, _transform.scale);
-			return model;}
-		
-		const Transform& getTransform() { return _transform; }
+			return model;
+		}
+
+		const Transform& getTransform() const { return _transform; }
 
 	private:
 		void cleanup();
