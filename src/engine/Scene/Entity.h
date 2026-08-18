68df2d02445f9921dccda3f39a42e185f3a4c7fa
bigman79 <64438880+bigman79@users.noreply.github.com>
2026-08-17 18:29:44 -0700

    Built some basic classes for entities and camera

---
+++ src/engine/Scene/Entity.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace Engine {
+	
+	struct Transform {
+		
+		glm::vec3 position{ 0.0f, 0.0f, 0.0f };
+		glm::vec3 rotation{ 0.0f, 0.0f, 0.0f };
+		glm::vec3 scale{ 1.0f, 1.0f, 1.0f };
+
+	};
+
+	class Entity {
+	public:
+		Entity(const Transform& transform)
+		: _transform{ transform }{}
+
+		const glm::mat4 getModelMatrix() { glm::mat4 model = glm::translate(glm::mat4(1.0f), _transform.position);
+			model = glm::rotate(model, glm::radians(_transform.rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+			model = glm::rotate(model, glm::radians(_transform.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+			model = glm::rotate(model, glm::radians(_transform.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+			model = glm::scale(model, _transform.scale);
+			return model;}
+		
+		const Transform& getTransform() { return _transform; }
+
+	private:
+		void cleanup();
+
+		Transform _transform;
+		// Connect to mesh and texture
+
+	};
+
+}
